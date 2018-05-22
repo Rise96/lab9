af8fa0579272a6621b8b9ca03a9fb924fddcc6a4
Rise <ritm-temp@hotmail.com>
2018-05-22 10:13:26 +0600

    Lab9 v1.0

---
+++ Lab9/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new List<List<int>>();
+            var row = new List<int>();
+            var rand = new Random();
+            int n = rand.Next(3, 6);
+
+            for (int i = 0; i < n; i++)
+            {
+                row = new List<int>();
+                for (int j = 0; j < n; j++)
+                {
+                    row.Add(rand.Next(-30, 30));
+                }
+
+                array.Add(row);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i][j].ToString() + " ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            var sorter = new Sorter();
+            List<List<int>> array2 = sorter.Sorting(array);
+
+            for (int i = 0; i < array2.Count; i++)
+            {
+                for (int j = 0; j < array2[i].Count; j++)
+                {
+                    Console.Write(array2[i][j].ToString() + " ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab9/Sorter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Sorter
+    {
+        public List<List<int>> Sorting(List<List<int>> array)
+        {
+            int k = FindColumnWithMax(array);
+            for(int i = 0; i < array.Count(); i++)
+            {
+                for(int j = i; j < array.Count(); j++)
+                {
+                    if (array[j][k] < array[i][k])
+                    {
+                        array.Insert(i, array[j]);
+                        array.RemoveAt(j+1);
+                    }
+                }
+            }
+            return array;
+        }
+        private int FindColumnWithMax(List<List<int>> array)
+        {
+            int k = 0;
+            int max = array[0][0];
+            for(int i = 0; i < array.Count(); i++)
+            {
+                for(int j = 0; j < array[i].Count(); j++)
+                {
+                    if (array[i][j] > max)
+                    {
+                        max = array[i][j];
+                        k = j;
+                    }
+                }
+            }
+            return k;
+        }
+    }
+}
